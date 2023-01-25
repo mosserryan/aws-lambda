7693245e9fb770582f02f15de4ecf17340f9a3b7
mosserryan <mosser.ryan.a@gmail.com>
2023-01-25 11:36:52 -0800

    Added the GetUserLambdaHandler with test, up and operational in AWS Lambda.

--- src/main/java/com/chairforce/handler/GetUserLambda.java
+++ src/main/java/com/chairforce/handler/GetUserLambda.java
@@ -1,42 +1,66 @@
 package com.chairforce.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.chairforce.dagger.DaggerConfig;
+import com.chairforce.entities.User;
 import com.chairforce.utilities.RequestObjectValidator;
 import com.chairforce.utilities.LambdaStatus;
 import com.chairforce.utilities.UserUtil;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-
-import javax.inject.Inject;
+import com.google.gson.JsonSyntaxException;
 import java.io.*;
-
+import java.util.Optional;
 
 public class GetUserLambda implements RequestStreamHandler {
 
-    @Inject UserUtil userUtil;
-
-    private static final Logger log = LogManager.getLogger(GetUserLambda.class);
     private final static LambdaStatus lambdaStatus = LambdaStatus.getInstance();
+    private final UserUtil userUtil;
 
+    public GetUserLambda() {
+        userUtil = DaggerConfig.create().userUtil();
+    }
 
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
-        OutputStreamWriter output = new OutputStreamWriter(outputStream);
+        LambdaLogger lambdaLogger = context.getLogger();
+        lambdaStatus.setLambdaLogger(lambdaLogger);
+
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream);
+        BufferedReader reader;
+        JsonObject jsonObject;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+        } catch (JsonSyntaxException e) {
+            lambdaLogger.log(e.getMessage());
+            return;
+        }
 
         if (!RequestObjectValidator.validateRequest(jsonObject)) {
             return;
         }
 
-        System.out.println(lambdaStatus.getRequestObj());
+        Optional<User> user = userUtil.getUserFromJson(lambdaStatus.getRequestObjAsString());
+        JsonObject responseBody = new JsonObject();
+        if (user.isEmpty()) {
+            lambdaStatus.log("Could not find specified user, with supplied input:");
+            responseBody.addProperty("statusCode", "404");
+        } else {
+            lambdaStatus.log("Successfully found the following user:" + user.get());
+            responseBody.addProperty("statusCode", "200");
+        }
 
-         System.out.println("It's not false!");
+        JsonObject responseObject = new JsonObject();
+        responseObject.addProperty("body", responseBody.toString());
+
+        writer.write(responseObject.toString());
+        writer.close();
+        reader.close();
 
     }
 
